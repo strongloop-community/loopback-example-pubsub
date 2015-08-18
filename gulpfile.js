9893df14e9da592f5262662e420858496500a4cb
Simon Ho <superkhau@gmail.com>
2015-08-18 14:51:20 -0700

    Add lb-services.js to .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var loopbackAngular = require('gulp-loopback-sdk-angular');
 
-gulp.task('default', function () {
+gulp.task('default', function() {
   return gulp.src('./server/server.js')
     .pipe(loopbackAngular())
     .pipe(rename('lb-services.js'))
